8f801a5297a12a364fb333e49765f193a84eabff
Texhnolyze47 <ichigobankai136@gmail.com>
2022-11-07 10:43:07 -0600

    se volvio a agregar los getters y setters del GithubUser.java

--- src/main/java/com/texhnolyze/githubapiv2/GithubUser.java
+++ src/main/java/com/texhnolyze/githubapiv2/GithubUser.java
@@ -5,4 +5,27 @@ public class GithubUser {
     private Integer id;
     private String url;
 
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
