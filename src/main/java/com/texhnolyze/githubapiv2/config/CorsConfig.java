39b64c27d89848a38c9847a1a72d638209729a12
Texhnolyze47 <ichigobankai136@gmail.com>
2022-12-27 03:42:00 -0600

    add: implementación de la configuración de cors global

--- src/main/java/com/texhnolyze/githubapiv2/config/CorsConfig.java
+++ src/main/java/com/texhnolyze/githubapiv2/config/CorsConfig.java
@@ -1,2 +1,16 @@
-package com.texhnolyze.githubapiv2.config;public class CorsConfig {
+package com.texhnolyze.githubapiv2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/users/**")
+                .allowedOrigins("https://escihu-wizards.netlify.app")
+                .allowedMethods("GET","POST");
+    }
 }
